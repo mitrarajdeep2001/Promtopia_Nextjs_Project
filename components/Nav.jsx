addc66a429ca9764838a34604f70246a8f93e421
Rajdeep Mitra <rajdeepmitra8@gmail.com>
2024-02-13 23:50:42 +0530

    Made improvements

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,12 +2,22 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { signOut } from "next-auth/react";
-import { useState } from "react";
+import { getProviders, signIn, signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const Nav = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
-  const isUserLoggedin = true;
+  const [providers, setProviders] = useState(null);
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      console.log(res);
+      setProviders(res);
+    };
+    fetchProviders();
+  }, []);
+  const { data: session } = useSession();
+  console.log("TEST", providers);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href={"/"} className="flex-center gap-2">
@@ -22,7 +32,7 @@ const Nav = () => {
       </Link>
       {/* Desktop Navigation */}
       <div className="hidden md:flex">
-        {isUserLoggedin ? (
+        {session ? (
           <div className="flex gap-3 md:gap-5">
             <Link href={"/create-prompt"} className="black_btn">
               Create Post
@@ -40,12 +50,23 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  key={provider.name}
+                  onClick={signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
         )}
       </div>
       {/* Mobile Navigation */}
       <div className="flex md:hidden">
-        {isUserLoggedin ? (
+        {session ? (
           <div className="flex flex-col">
             <Image
               src={"/assets/images/profile-pic.png"}
@@ -56,49 +77,54 @@ const Nav = () => {
             />
             {/* Dropdown menu */}
             {toggleDropdown && (
-              <div className="z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                <ul
-                  className="py-2 text-sm text-gray-700 dark:text-gray-400"
-                  aria-labelledby="dropdownLargeButton"
-                >
+              <div className="z-10 absolute top-14 right-7 shadow-xl font-normal bg-white divide-y divide-gray-100 rounded-lg w-44 dark:bg-gray-700 dark:divide-gray-600">
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-400">
                   <li>
-                    <a
-                      href="#"
+                    <Link
+                      href="/profile"
+                      onClick={() => setToggleDropdown(false)}
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
-                      Dashboard
-                    </a>
+                      My Profile
+                    </Link>
                   </li>
                   <li>
-                    <a
-                      href="#"
+                    <Link
+                      href="create-prompt"
+                      onClick={() => setToggleDropdown(false)}
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Earnings
-                    </a>
+                      Create Prompt
+                    </Link>
                   </li>
                 </ul>
                 <div className="py-1">
-                  <a
-                    href="#"
+                  <button
+                    onClick={() => {
+                      setToggleDropdown(false);
+                      signOut();
+                    }}
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                   >
                     Sign out
-                  </a>
+                  </button>
                 </div>
               </div>
             )}
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  key={provider.name}
+                  onClick={signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
